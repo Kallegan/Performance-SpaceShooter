2f8a981495f7bd8fe912985528eff64e7293ef40
Kallegan <90606627+Kallegan@users.noreply.github.com>
2022-12-14 21:52:50 +0100

    asteroid model tweak

--- Assets/Scripts/Asteroid/AsteroidField.cs
+++ Assets/Scripts/Asteroid/AsteroidField.cs
@@ -6,7 +6,7 @@ public class AsteroidField : MonoBehaviour
 {
     [SerializeField] [Range(100, 10000)] private int _asteroidCount;
     [SerializeField] [Range(100f, 500f)] private float _radius = 300f;
-    [SerializeField] [Range(0.5f, 5f)] private float _maxScale = 5;
+    [SerializeField] [Range(0.5f, 100f)] private float _maxScale = 100;
     [SerializeField] List<GameObject> _asteroidPrefabs;
 
     private Transform _transform;
@@ -27,7 +27,7 @@ public class AsteroidField : MonoBehaviour
         for (int i = 0; i < _asteroidCount; i++)
         {
             GameObject asteroid = Instantiate(_asteroidPrefabs[Random.Range(0, _asteroidPrefabs.Count)], _transform.position, Quaternion.identity);
-            float scale = Random.Range(0.5f, _maxScale);
+            float scale = Random.Range(30f, _maxScale);
             asteroid.transform.localScale = new Vector3(scale, scale, scale);
             asteroid.transform.position += Random.insideUnitSphere * _radius;
             asteroid.GetComponent<Rigidbody>()?.AddTorque(Random.insideUnitCircle * Random.Range(0f, 50f));
